1d9050c2cd4734cb31e6724401c14eb449531e1e
esilv-evd <esilv-evd@devinci.online>
2021-01-11 02:03:20 +0100

    Add some comments in MCP9700 header

--- envmon/lib/mcp9700/mcp9700.h
+++ envmon/lib/mcp9700/mcp9700.h
@@ -32,11 +32,13 @@
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
 
+/* macro defintions */
 #define ADC_WIDTH      ADC_WIDTH_BIT_12
 #define ADC_ATTEN      ADC_ATTEN_DB_0
 #define DEFAULT_VREF   1100
 #define NO_OF_SAMPLES  64
 
+/* structure definitions */
 typedef struct mcp9700
 {
     adc_unit_t unit;
@@ -44,7 +46,7 @@ typedef struct mcp9700
     esp_adc_cal_characteristics_t adc_chars;
 } mcp9700_t;
 
-
+/* function prototypes */
 void    mcp9700_init(adc_unit_t, adc_channel_t);
 int32_t mcp9700_get_value();
 
