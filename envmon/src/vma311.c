91c0d72039547803eade579a928668770b8f2066
esilv-evd <esilv-evd@devinci.online>
2022-02-14 16:20:23 +0100

    Remove holes.

--- envmon/src/vma311.c
+++ envmon/src/vma311.c
@@ -27,8 +27,12 @@
  */
 
 /**
+ * @file vma311.c
+ * @author Frédéric Fauberteau
+ * @brief Implementation of VMA311 (based on DHT11) sensor driver.
+ * 
  * This code is inspired by:
- * https://github.com/0nism/esp32-DHT11/blob/master/dht11.c
+ * @see https://github.com/0nism/esp32-DHT11/blob/master/dht11.c
  */
 
 #include "vma311.h"
@@ -37,34 +41,20 @@
 
 static vma311_t vma311;
 
-/* static function prototypes */
 static        void            vma311_send_start_signal();
 static        int             vma311_wait(uint16_t, int);
 static        int             vma311_check_response();
 static inline vma311_status_t vma311_read_byte(uint8_t *);
 static        vma311_status_t vma311_check_crc(uint8_t *);
 
-/**
- * Initialize VMA311.
- *
- * 1. Reset the GPIO pins
- *    (https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/gpio.html#_CPPv414gpio_reset_pin10gpio_num_t)
- * 2. Delay 1 second until the sensor becomes stable
- */
 void vma311_init(gpio_num_t num)
 {
     vma311.num = num;
     vma311.last_read_time = -2000000;
-    /* 1. */
-    /* 2. */
+    gpio_reset_pin(vma311.num);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
 
-/**
- * Get the values send by the VMA311 sensor.
- * (Page 5 of https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf)
- * Complete:
- * 1. The field affectation
- */
 vma311_data_t vma311_get_values()
 {
     vma311_data_t error_data = {VMA311_TIMEOUT_ERROR, -1, -1, -1, -1};
@@ -89,15 +79,16 @@ vma311_data_t vma311_get_values()
     }
     if(vma311_check_crc(data) != VMA311_CRC_ERROR)
     {
-      /* 1. */
+        vma311.data.status = VMA311_OK;
+        vma311.data.rh_int = data[0];
+        vma311.data.rh_dec = data[1];
+        vma311.data.t_int = data[2];
+        vma311.data.t_dec = data[3];
     }
     return vma311.data;
 }
 
-/**
- * Send start signal to VMA311.
- */
-void vma311_send_start_signal()
+static void vma311_send_start_signal()
 {
     gpio_set_direction(vma311.num, GPIO_MODE_OUTPUT);
     gpio_set_level(vma311.num, 0);
@@ -106,10 +97,7 @@ void vma311_send_start_signal()
     ets_delay_us(40);
 }
 
-/**
- * Wait for a number of microseconds at a given level.
- */
-int vma311_wait(uint16_t us, int level)
+static int vma311_wait(uint16_t us, int level)
 {
     int us_ticks = 0;
 
@@ -124,44 +112,29 @@ int vma311_wait(uint16_t us, int level)
     return us_ticks;
 }
 
-/**
- * Check the response of the VMA311 sensor.
- * (Page 6 of https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf)
- * Complete:
- * 1. The duration
- * 2. The level
- */
-int vma311_check_response()
+static int vma311_check_response()
 {
     gpio_set_direction(vma311.num, GPIO_MODE_INPUT);
-    if(vma311_wait(/* 1. */, /* 2. */) == VMA311_TIMEOUT_ERROR)
+    if(vma311_wait(80, 0) == VMA311_TIMEOUT_ERROR)
     {
         return VMA311_TIMEOUT_ERROR;
     }
-    if(vma311_wait(/* 1. */, /* 2. */) == VMA311_TIMEOUT_ERROR) 
+    if(vma311_wait(80, 1) == VMA311_TIMEOUT_ERROR) 
     {
         return VMA311_TIMEOUT_ERROR;
     }
     return VMA311_OK;
 }
 
-/**
- * Read 8 bits from the GPIO.
- * (Page 7 and 8 of https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf)
- * Complete:
- * 1. The duration
- * 2. The level
- * 3. The max duration of a low signal
- */
-vma311_status_t vma311_read_byte(uint8_t *byte)
+static vma311_status_t vma311_read_byte(uint8_t *byte)
 {
     for (int i = 0; i < 8; ++i)
     {
-        if(vma311_wait(/* 1. */, /* 2. */) == VMA311_TIMEOUT_ERROR)
+        if(vma311_wait(50, 0) == VMA311_TIMEOUT_ERROR)
         {
             return VMA311_TIMEOUT_ERROR;
         }
-        if(vma311_wait(/* 1. */, /* 2. */) > /* 3. */)
+        if(vma311_wait(70, 1) > 28)
         {
             *byte |= (1 << (7 - i));
         }
@@ -169,14 +142,10 @@ vma311_status_t vma311_read_byte(uint8_t *byte)
     return VMA311_OK;
 }
 
-/**
- * 
- * Chech the checksum to verify that data are consistent.
- * Complete:
- * 1. Compute the checksum and return VMA311_OK or VMA311_CRC_ERROR
- * (Page 5 of https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf)
- */
-vma311_status_t vma311_check_crc(uint8_t data[])
-{
-  /* 1. */
+static vma311_status_t vma311_check_crc(uint8_t data[]) {
+    if(data[4] != (data[0] + data[1] + data[2] + data[3]))
+    {
+        return VMA311_CRC_ERROR;
+    }
+    return VMA311_OK;
 }
